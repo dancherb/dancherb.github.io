b8eecac43b7c11ef4bdbc129184d0b9ea9fcee56
dancherb <dan674@hotmail.co.uk>
2017-10-01 20:11:17 +0100

    a

--- map/mapscript.js
+++ map/mapscript.js
@@ -1,19 +1,29 @@
 var c=document.getElementById("canvas");
 var tiles = []
-var totaltiles = 40;
+var tilesize = 25
+console.log(tilesize);
+// mapwidth and mapheight in tiles
+var mapwidth = c.width/tilesize
+var mapheight = c.height/tilesize
+var totaltiles =  mapwidth*mapheight
+console.log(mapwidth, mapheight, totaltiles)
 for(i=0; i<totaltiles; i++) {
   tiles[i]=c.getContext("2d");
-  tiles[i].land = Math.floor((Math.random() * 2) + 1)
-  if(tiles[i].land === 1) {
+  tiles[i].land = Math.floor((Math.random() * 10) + 1)
+  if(tiles[i].land <= 5) {
     //green
     tiles[i].fillStyle="#87D652";
   } else {
     //blue
     tiles[i].fillStyle="#4AABD6"
   }
-  tiles[i].fillRect(i*25, 0, 25, 25);
+  tiles[i].xpos = tilesize*(i%mapwidth)
+  tiles[i].ypos = tilesize*(Math.floor(i/mapwidth))
+  tiles[i].fillRect(tiles[i].xpos, tiles[i].ypos, tilesize, tilesize);
 }
-
+// 0(1) 0(2) 0(3) 0(4) 0(5)
+// 1(6) 1(7) 1 1 1
+// 2(11) 2 2 2 2
 // refer to individual tile
 // tiles[6].fillStyle="#000000"
 // tiles[6].fillRect(6*25,0, 25, 25);
