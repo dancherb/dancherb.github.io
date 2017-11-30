9c942dc61ad4c6003de74b450345f29345e6db49
dancherb <dan674@hotmail.co.uk>
2017-11-30 02:46:21 +0000

    added 3D background option (/?3d=on)

---
+++ 3script.js
@@ -0,0 +1,80 @@
+function start3D() {
+
+var scene = new THREE.Scene()
+var renderer = new THREE.WebGLRenderer()
+
+camera = new THREE.PerspectiveCamera(
+    35, // angle
+    window.innerWidth / window.innerHeight, // aspect ratio
+    1, // near plane (range to render)
+    1000 // far plane
+);
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild( renderer.domElement);
+
+scene.background = new THREE.Color( 0xe4e4e4 );
+
+var light = new THREE.AmbientLight(0xffffff)
+scene.add(light)
+
+var boxCount = 10
+var box = {}
+for(i=0; i<boxCount; i++) {
+    var boxSize = randomNum(2, 20)
+    var whiteLevel = Math.floor(randomNum(170, 245))
+    box[i] = {}
+    box[i].object = new THREE.Mesh(
+        new THREE.BoxGeometry(boxSize, boxSize, boxSize),
+        new THREE.MeshBasicMaterial({color:`rgb(${whiteLevel}, ${whiteLevel}, ${whiteLevel})`})
+    )
+
+    var boxSpin = randomNum(-0.05, 0.05)
+    var boxSpinAxis = Math.floor(randomNum(1,3))
+    box[i].boxSpinX = boxSpinAxis === 1 ? boxSpin : 0
+    box[i].boxSpinY = boxSpinAxis === 2 ? boxSpin : 0
+    box[i].boxSpinZ = boxSpinAxis === 3 ? boxSpin : 0
+
+    box[i].object.position.set(randomNum(-30, 30), randomNum(-30, 30), randomNum(-30, 30));
+    // box[i].object.position.set(randomNum(-100, 100), randomNum(-100, 100), randomNum(-100, 100));
+    scene.add(box[i].object)
+}
+
+camera.position.z = 100;
+scene.add(camera);
+
+function render() {
+    // box[4].object.position.x += 1
+    // for(i=0; i<boxCount; i++) {
+    //     box[i].object.rotation.x += box[i].boxSpinXbox
+    //     box[i].object.rotation.y += box[i].boxSpinY
+    //     box[i].object.rotation.z += box[i].boxSpinZ
+    // }
+    camera.rotation.z += 0.004
+    // camera.rotation.y += 0.001
+    requestAnimationFrame(render);
+    renderer.render(scene, camera);
+}
+render()
+
+function randomNum(max = 30, min = 2) {
+    var range = max-min
+    return (Math.random()*range)+min;
+}
+function randomTrue() {
+    return Math.random()>0.5 ? true : false;
+}
+
+}
+
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    return vars;
+}
+var three = getUrlVars()["3D"];
+if(three === true || three == "on") {
+    start3D()
+}
--- homescript.js
+++ homescript.js
@@ -1,45 +1,47 @@
-var projectsOpen = false;
-projectsText.style.fontSize = "0px" //keep 'projects' title invisible and tiny initially
-projectsText.style.opacity = 0;
+function start3D() {
+  var projectsOpen = false;
+  projectsText.style.fontSize = "0px" //keep 'projects' title invisible and tiny initially
+  projectsText.style.opacity = 0;
 
-function openProjects() {
-  if(projectsOpen === false) {
-    // projectsContain.style.position = "relative"
-    projectsText.style.fontSize = "35px"
-    projectsText.style.opacity = 1
-    projectsText.style.transition = "all 0.3s ease";
-    // and then after a delay, open the projects
-    setTimeout(openList, 300)
-    function openList() {
-      project1.className = "projectO"
-      project2.className = "projectO"
-      project3.className = "projectO"
-      project4.className = "projectO"
+  function openProjects() {
+    if(projectsOpen === false) {
+      // projectsContain.style.position = "relative"
+      projectsText.style.fontSize = "35px"
+      projectsText.style.opacity = 1
+      projectsText.style.transition = "all 0.3s ease";
+      // and then after a delay, open the projects
+      setTimeout(openList, 300)
+      function openList() {
+        project1.className = "projectO"
+        project2.className = "projectO"
+        project3.className = "projectO"
+        project4.className = "projectO"
+      }
+      container.style.paddingTop = "0px"
+      //
+      toggleIcon()
+      projectsOpen = true
+    } else {
+      projectsText.style.fontSize = "0px"
+      projectsText.style.opacity = 0
+      projectsText.style.transition = "all 0.3s ease 0.3s";
+      //
+      project1.className = "projectC"
+      project2.className = "projectC"
+      project3.className = "projectC"
+      project4.className = "projectC"
+      //
+      container.style.paddingTop  = "100px"
+      //
+      setTimeout(toggleIcon, 650)
+      projectsOpen = false
     }
-    container.style.paddingTop = "0px"
-    //
-    toggleIcon()
-    projectsOpen = true
-  } else {
-    projectsText.style.fontSize = "0px"
-    projectsText.style.opacity = 0
-    projectsText.style.transition = "all 0.3s ease 0.3s";
-    //
-    project1.className = "projectC"
-    project2.className = "projectC"
-    project3.className = "projectC"
-    project4.className = "projectC"
-    //
-    container.style.paddingTop  = "100px"
-    //
-    setTimeout(toggleIcon, 650)
-    projectsOpen = false
+    container.style.transition = "all 0.8s ease";
   }
-  container.style.transition = "all 0.8s ease";
-}
-function toggleIcon() {
-  projectsIcon.classList.toggle("fa-envelope-open-o");
-  projectsIcon.classList.toggle("fa-envelope-o");
+  function toggleIcon() {
+    projectsIcon.classList.toggle("fa-envelope-open-o");
+    projectsIcon.classList.toggle("fa-envelope-o");
+  }
 }
 
 // check if projects should start open (e.g. if coming straight back from flash page)
